c5d3d7e720eeb1315e4d0a5ff7f6a4b03a8888fa
Fridry <fridry.ed@hotmail.com>
2023-02-05 20:51:33 -0300

    Save file to S3

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import axios from "axios";
 import fs from "fs/promises";
 import { randomUUID } from "crypto";
 import { Parser } from "@json2csv/plainjs";
+import AWS from "aws-sdk";
+
+AWS.config.update({region: 'us-west-1'});
+
+const s3 = new AWS.S3({ apiVersion: "latest" });
 
 export const handler = async (event, context) => {
   try {
+    const filename = `output-${randomUUID()}.csv`
+
     let url = "https://dummyjson.com/products";
 
     if (event?.id) {
@@ -15,23 +22,21 @@ export const handler = async (event, context) => {
 
     const data = convertToCsv(response?.data?.products);
 
-    await saveFile(data);
+    await saveFile(data, filename);
+
+    await uploadFileOnS3(filename);
 
     return {
-        message: "Success"
-    }
+      message: "Success",
+    };
   } catch (error) {
     console.error(error);
   }
 };
 
-const saveFile = async (data) => {
+const saveFile = async (data, filename) => {
   try {
-    await fs.writeFile(
-      `/tmp/output-${randomUUID()}.csv`,
-      data,
-      "utf-8"
-    );
+    await fs.writeFile(`/tmp/${filename}`, data, "utf-8");
 
     console.log("Done");
   } catch (error) {
@@ -42,9 +47,9 @@ const saveFile = async (data) => {
 const convertToCsv = (data) => {
   try {
     const parser = new Parser({
-        delimiter: ";"
+      delimiter: ";",
     });
-    
+
     const csv = parser.parse(data);
 
     return csv;
@@ -52,3 +57,26 @@ const convertToCsv = (data) => {
     console.error(error);
   }
 };
+
+const uploadFileOnS3 = async (fileName) => {
+  const fileContent = await fs.readFile(`/tmp/${fileName}`);
+
+  const params = {
+    Bucket: "aws-s3-bucket-tutorials",
+    Key: fileName,
+    Body: fileContent,
+    ContentType: 'text/csv'
+  };
+
+  try {
+    const response = await s3.upload(params).promise();
+
+    console.log("Response: ", response);
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+handler().then(data => data).catch(error => console.error(error))
