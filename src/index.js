dcc41dbfaf10641393f6497619fbc1a125cc156f
Fridry <fridry.ed@hotmail.com>
2023-02-05 19:04:45 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import fs from "fs/promises";
+import { randomUUID } from "crypto";
+import { Parser } from "@json2csv/plainjs";
+
+export const handler = async (event, context) => {
+  try {
+    let url = "https://dummyjson.com/products";
+
+    if (event?.id) {
+      url = `https://dummyjson.com/products/${event?.id}`;
+    }
+
+    const response = await axios.get(url);
+
+    const data = convertToCsv(response?.data?.products);
+
+    await saveFile(data);
+
+    return {
+        message: "Success"
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const saveFile = async (data) => {
+  try {
+    await fs.writeFile(
+      `./tmp/output-${randomUUID()}.csv`,
+      data,
+      "utf-8"
+    );
+
+    console.log("Done");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const convertToCsv = (data) => {
+  try {
+    const parser = new Parser({
+        delimiter: ";"
+    });
+    
+    const csv = parser.parse(data);
+
+    return csv;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+handler()
+  .then((data) => data)
+  .catch((error) => console.error(error));
